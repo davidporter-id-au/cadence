545be9d9fd761570cee6954cc01ff713670ce56e
David Porter <david.porter@uber.com>
2026-01-12 18:16:49 -0800

    adding logs

--- service/history/engine/engineimpl/start_workflow_execution.go
+++ service/history/engine/engineimpl/start_workflow_execution.go
@@ -371,6 +371,13 @@ func (e *historyEngineImpl) handleCreateWorkflowExecutionFailureCleanup(
 ) {
 
 	if !e.shard.GetConfig().EnableCleanupOrphanedHistoryBranchOnWorkflowCreation(domainEntry.GetInfo().Name) {
+		e.logger.Warn("cleanup of orphaned history branch is disabled, but possible orphaned history branch was detected",
+			tag.WorkflowDomainID(domainEntry.GetInfo().ID),
+			tag.WorkflowID(workflowExecution.WorkflowID),
+			tag.WorkflowRunID(workflowExecution.RunID),
+			tag.Dynamic("debug-isSignalWithStart", isSignalWithStart),
+			tag.Error(err),
+		)
 		return
 	}
 
